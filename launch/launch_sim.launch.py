0991091c1a6d8fe95df8d9559976e80f2aba0302
fire_enigma <ee23b061@smail.iitm.ac.in>
2024-08-06 20:15:22 +0530

    Created world file and integrated it into the launch file

--- launch/launch_sim.launch.py
+++ launch/launch_sim.launch.py
@@ -2,19 +2,13 @@ import os
 
 from ament_index_python.packages import get_package_share_directory
 
-
 from launch import LaunchDescription
 from launch.actions import IncludeLaunchDescription
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 
 from launch_ros.actions import Node
 
-
-
 def generate_launch_description():
-
-
-   
     package_name='primebot_1' 
 
     rsp = IncludeLaunchDescription(
@@ -27,6 +21,7 @@ def generate_launch_description():
     gazebo = IncludeLaunchDescription(
                 PythonLaunchDescriptionSource([os.path.join(
                     get_package_share_directory('gazebo_ros'), 'launch', 'gazebo.launch.py')]),
+                launch_arguments={'world': os.path.join(get_package_share_directory(package_name), 'worlds', 'line_world.world')}.items()
              )
 
     # Run the spawner node from the gazebo_ros package. The entity name doesn't really matter if you only have a single robot.
@@ -35,9 +30,6 @@ def generate_launch_description():
                                    '-entity', 'my_bot'],
                         output='screen')
 
-
-
-    
     return LaunchDescription([
         rsp,
         gazebo,
