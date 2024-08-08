c96b6ffdfca4a17ade2c9742ed87a61fb353b9a5
fire_enigma <ee23b061@smail.iitm.ac.in>
2024-08-08 20:02:01 +0530

    Created infrared sensors and plugins .

--- launch/launch_sim.launch.py
+++ launch/launch_sim.launch.py
@@ -24,7 +24,7 @@ def generate_launch_description():
                 launch_arguments={'world': os.path.join(get_package_share_directory(package_name), 'worlds', 'line_world.world')}.items()
              )
 
-    # Run the spawner node from the gazebo_ros package. The entity name doesn't really matter if you only have a single robot.
+    # Run the spawner node from the gazebo_ros package.
     spawn_entity = Node(package='gazebo_ros', executable='spawn_entity.py',
                         arguments=['-topic', 'robot_description',
                                    '-entity', 'my_bot'],
